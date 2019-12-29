33d96525e520a145a223ac231f88a9b9012c81bb
Fergal <fergal.mullally@gmail.com>
2019-12-29 11:35:44 -0500

    Moving general purpose code away

--- fileio/kplrfits.py
+++ fileio/kplrfits.py
@@ -1,10 +1,9 @@
     # -*- coding: utf-8 -*-
 
 
-#Note: binData is imported later in the file.
-#This allows me a fall back to the slow code if it can't be found.
+import dave.misc.transitfunc as transitfunc
+import dave.fileio.nca as nca
 import numpy as np
-import nca
 
 
 
@@ -171,8 +170,8 @@ class KeplerQuarter():
 def loadMultiQuarterPdc(archive, kepid, quarterList, \
     fitCubic=True, removeEarthPoint=False):
 
-    print "WARNING loadMultiQuarterPdc deprecated. Use"
-    print "loadMultiQuarter() instead"
+    print ("WARNING loadMultiQuarterPdc deprecated. Use")
+    print ("loadMultiQuarter() instead")
     return loadMultiQuarter(archive, kepid, quarterList, fitCubic=fitCubic, \
         removeEarthPoint=removeEarthPoint)
 
@@ -226,9 +225,9 @@ def loadMultiQuarter(archive, kepid, quarterList, \
         #Load file
         try:
             fits = archive.getLongCadence(kepid, q)
-        except IOError, e:
-            print "WARN: Failed to load data for %i Q%i" %(int(kepid), q)
-            print e
+        except IOError as e:
+            print ("WARN: Failed to load data for %i Q%i" %(int(kepid), q))
+            print (e)
             continue
 
         data = getNumpyArrayFromFitsRec(fits)
@@ -338,7 +337,8 @@ def getIndicesOfGoodData(data):
     zero crossing, spsd, or cosmic rays in the calibration data.
     These data may indicate lower quality data.
 
-
+    A Kepler specific function
+    
     Input:
     data:    (2d numpy array) Data to be smoothed, as loaded from loadData()
 
@@ -351,23 +351,12 @@ def getIndicesOfGoodData(data):
     flag = data[:, FluxColDef['SAP_QUALITY']]
 
     #Set a mask that matches everything but listed flags.
-    m = getMaskForBadData()
-    mask = np.zeros( (len(flag)), dtype=np.uint32) + m
-
-    #Find any cadences that match flag
-    idx = np.bitwise_and(flag.astype(np.uint32), mask)
-    idx = np.where(idx>0, 0, 1)    #Convert to booleans
-
-    #Also match cadences where we don't flag but set either
-    #time or flux to NaN
-    idx = np.bitwise_and(idx, np.where(np.isfinite(time), 1, 0))
-    idx = np.bitwise_and(idx, np.where(np.isfinite(flux), 1, 0))
-
-    #Return only the indices set to true
+    m = getMaskForBadKeplerData()
+    idx = transitfunc.getIndicesOfGoodData(time, flux, flag, m)
     return np.where(idx)[0]
 
 
-def getMaskForBadData():
+def getMaskForBadKeplerData():
     """If data[i, 'SAP_QUALITY] & m, data is marked with a flag
        indicating it's probably, or definiately bad.
 
@@ -462,31 +451,7 @@ def fitAndRemovePolynomial(data, order, \
 
 
 def medianZero(data, col):
-    """If x= data[:,col], set x = (x/median(x) - 1
-
-        Note:
-        This function is preferable to meanZero when the data has outliers.
-    """
-
-    med = np.median(data[:,col])
-
-    tol=1e-9
-    if np.fabs(med) < tol:
-        raise ValueError("Median of lightcurve already nearly zero")
-
-    data[:,col] /= med
-    data[:,col] -= 1
-    return data
-
-
-
-###################################################
-
-
-
-
-
-
+    return transitfunc.medianZero(data, col)
 
 
 def old_foldAndBinData(x, y, period, epoch, expTime, numBins):
@@ -546,74 +511,17 @@ def old_foldAndBinData(x, y, period, epoch, expTime, numBins):
 try:
     from binData import foldAndBinData
 except ImportError:
-    print "INFO: binData.so not found. Falling back on old_foldAndBinnedData"
+    print("INFO: binData.so not found. Falling back on old_foldAndBinnedData")
     foldAndBinData = old_foldAndBinData
-
 
 
 def markTransitCadences(time, period_days, epoch_bkjd, duration_days,\
     numberOfDurations=1, flags=None):
-    """Create a logical array indicating which cadences are
-    affected by a transit
-
-    Input:
-    -------------
-    time:
-        (numpy 1d array) array of cadence times
-    period_days:
-        Transit period
-    epoch_bkjd:
-        Transit epoch
-    duration_days:
-        Duration of transit (start to finish). If you
-        select a duration from first to last contact,
-        all cadences affected by transit are selected.
-        If you only select 2 to 3 contact, only the
-        interior region of the transit is selected
-    numberOfDurations
-        How much of the lightcurve either side of
-        the transit to mark. Default means to mark
-        1/2 the transit duration either side of
-        transit center.
-
-    Optional Inputs:
-    flags:
-        If set, must be an array of booleans of length ``time``.
-        Cadences where this array is true are ignored in the
-        calculation. This is useful if some of the entries of time
-        are Nan.
-
-    Returns:
-    -------------
-    Array of length len(time) of booleans. Element set to true
-    are affected by transits
-    """
-
-    if flags is None:
-        flags = np.zeros_like(time, dtype=bool)
-
-    i0 = np.floor((np.min(time[~flags])-epoch_bkjd)/period_days)
-    i1 =  np.ceil((np.max(time[~flags])-epoch_bkjd)/period_days)
-    assert(np.isfinite(i0))
-    assert(np.isfinite(i1))
-
-    irange = np.arange(i0, i1+1)
-    transitTimes = epoch_bkjd + period_days*irange
-
-    idx = np.zeros( len(time), dtype=np.bool8)
-    for tt in transitTimes:
-        diff = time - tt
-        diff[flags] = 1e99  #A large value that isn't Nan
-        assert np.all(np.isfinite(diff)), "Nan found in diff"
-
-        idx = np.bitwise_or(idx, np.fabs(diff) < \
-            .5*duration_days*numberOfDurations)
-
-    if not np.any(idx):
-        print "WARN: No cadences found matching transit locations"
-    return idx
-
-
+    return transitfunc.markTransitCadences(time, period_days,
+                                           epoch_bkjd,
+                                           duration_days,
+                                           numberOfDurations,
+                                           flags)
 
 def regularSpaceKeplerDataByCadence(data, missingValue=0,
     cCol=FluxColDef['CADENCENO']):
